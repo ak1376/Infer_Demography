fa661f91b027ed91f453820f98876679cd53f15b
ak1376 <akapoor@uoregon.edu>
2025-12-10 10:56:15 -0800

    Making sure that the train,tune, and validation set are completely disjoint

--- bash_scripts/LD_stats_windows.sh
+++ bash_scripts/LD_stats_windows.sh
@@ -6,8 +6,9 @@
 #SBATCH --time=18:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --partition=kern,preempt,kerngpu 
+#SBATCH --partition=kerngpu
 #SBATCH --account=kernlab
+#SBATCH --gres=gpu:1
 #SBATCH --requeue
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=akapoor@uoregon.edu
@@ -20,8 +21,6 @@ BATCH_SIZE=50     # number of (sim,window) jobs per array task
 # ----------------------------------------------------------------------------
 
 # -------- config & constants -----------------------------------------------
-# : "${CFG_PATH:?CFG_PATH is not defined}"
-# CFG="$CFG_PATH"
 CFG="/home/akapoor/kernlab/Infer_Demography/config_files/experiment_config_split_migration.json"
 ROOT="/projects/kernlab/akapoor/Infer_Demography"
 SNAKEFILE="$ROOT/Snakefile"
@@ -44,7 +43,15 @@ for IDX in $(seq "$START" "$END"); do
     WIN=$(( IDX % NUM_WINDOWS ))
 
     TARGET="experiments/${MODEL}/inferences/sim_${SID}/MomentsLD/LD_stats/LD_stats_window_${WIN}.pkl"
-    echo "LD‑stats: SID=$SID  WIN=$WIN  →  $TARGET"
+    ABS_TARGET="${ROOT}/${TARGET}"
+
+    # 🔍 Skip if LD_stats already exists
+    if [[ -f "$ABS_TARGET" ]]; then
+        echo "SKIP: LD-stats already exist for SID=$SID WIN=$WIN ($ABS_TARGET)"
+        continue
+    fi
+
+    echo "RUN: LD-stats: SID=$SID  WIN=$WIN  →  $TARGET"
 
     snakemake --snakefile "$SNAKEFILE" \
               --directory  "$ROOT"      \
